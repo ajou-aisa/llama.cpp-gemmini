f72c3a724fb8920c1091d85a74b4ed1ddb532b48
zerogod <code0.sin@gmail.com>
2025-08-11 21:02:44 +0900

    fix: fix gemmini.h include error

--- ggml/src/ggml-gemmini/test/ggml-gemmini-test.h
+++ ggml/src/ggml-gemmini/test/ggml-gemmini-test.h
@@ -1,8 +1,9 @@
+// test/ggml-gemmini-test.h
 #ifndef __GGML_GEMMINI_TEST_H__
 #define __GGML_GEMMINI_TEST_H__
 
-#include "../ggml-gemmini-util.h"
 #include "include/gemmini.h"
+
 
 template <typename T>
 static inline void dump_matrix(const char* name, const T* m, int r, int c, int s);
@@ -10,7 +11,5 @@ static inline void dump_matrix(const char* name, const T* m, int r, int c, int s
 static inline int8_t sat_i8(int x) {
     return x > 127 ? 127 : (x < -128 ? -128 : (int8_t)x);
 }
-
-void ggml_backend_gemmini_mul_mat_test();
 
 #endif // __GGML_GEMMINI_TEST_H__
