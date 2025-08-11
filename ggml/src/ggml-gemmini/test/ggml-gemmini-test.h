1ded4ae072549664a6714d5ba0004a607d6bc233
zerogod <code0.sin@gmail.com>
2025-08-12 04:40:11 +0900

    fix: make test header only

--- ggml/src/ggml-gemmini/test/ggml-gemmini-test.h
+++ ggml/src/ggml-gemmini/test/ggml-gemmini-test.h
@@ -3,10 +3,33 @@
 #define __GGML_GEMMINI_TEST_H__
 
 #include "include/gemmini_params.h"
-
+#include <cstdio>
+#include <type_traits>
 
 template <typename T>
-static inline void dump_matrix(const char* name, const T* m, int r, int c, int s);
+static inline void dump_matrix(const char* name, const T* m, int r, int c, int s) {
+    printf("%s =\n", name);
+    for (int i = 0; i < r; ++i)
+    {
+        printf("[ ");
+        for (int j = 0; j < c; ++j)
+        {
+            const T v = m[i * s + j];
+            if constexpr (std::is_integral_v<T>)
+            {
+                if constexpr (sizeof(T) <= sizeof(int))
+                    printf("%d ", (int)v);
+                else
+                    printf("%lld ", (long long)v);
+            }
+            else
+            {
+                printf("%g ", (double)v);
+            }
+        }
+        printf("]\n");
+    }
+};
 
 static inline int8_t sat_i8(int x) {
     return x > 127 ? 127 : (x < -128 ? -128 : (int8_t)x);
