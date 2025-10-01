5fde1e5cc1305d0af9ae3e3186627450a7e5acad
zerogod <code0.sin@gmail.com>
2025-10-01 19:57:16 +0900

    fix: test call

--- ggml/src/ggml-gemmini/ggml-gemmini.cpp
+++ ggml/src/ggml-gemmini/ggml-gemmini.cpp
@@ -20,7 +20,7 @@ static void ggml_backend_gemmini_mul_mat(ggml_backend_gemmini_context *ctx,
 /* ________________ Test: 테스트 호출용 ___________________ */
 #if TEST
     static_assert(TRANSPOSE_B == 1, "This test assumes physical-transposed B (KxJ).");
-    ggml_gemmini_test(ctx, dst, bias);
+    ggml_gemmini_test(ctx, dst);
     return;
 #endif
 
