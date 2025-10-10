4489882d641b57e3e61730829ab4617a8c8dea9b
zerogod <code0.sin@gmail.com>
2025-10-11 02:10:50 +0900

    feat: compensation 적용 별도 함수로 분리 및 J_ 멤버화

--- ggml/src/ggml-gemmini/gemmini_tensor/error_compensation/activation_DEC.h
+++ ggml/src/ggml-gemmini/gemmini_tensor/error_compensation/activation_DEC.h
@@ -22,20 +22,26 @@ namespace aisa
     private:
         const ggml_tensor *A_;          // F32 activation
         const BenchTensor<int8_t> *qA_; // quantized activation
+        const BenchTensor<int8_t> *qW_; // quantized weight
+
         const char* layer_ = "others"; // layer
         size_t I_;  // number of rows  
         size_t K_;  // columns per row
+        size_t J_;  // number of output columns
         size_t alpha_;             // number of salient input channels
 
         std::vector<std::vector<int>> S_;       // row 별 outlier 인덱스 집합
         std::vector<std::vector<float>> delta_; // row 별 residual
         std::vector<std::pair<float, size_t>> sorted_;
 
-        ActivationDEC(const ggml_tensor *A, const BenchTensor<int8_t> *qA) : A_(A), qA_(qA) {}
+        ActivationDEC(const ggml_tensor *A, 
+                      const BenchTensor<int8_t> *qA, 
+                      const BenchTensor<int8_t> *qW) : A_(A), qA_(qA), qW_(qW){}
         void prepare();
+        void selectTopKandComputeResidual(size_t row_idx, const float *row_fp, const int8_t *row_q);
         void computeCompensation(const int8_t *W, size_t J, float *y_out);
         void computeCompensation_kGrouped(const int8_t *W, size_t J, float *y_com);
-        void selectTopKandComputeResidual(size_t row_idx, const float *row_fp, const int8_t *row_q);
+        void applyCompensation(ggml_tensor *C_out, const std::vector<float> &y_com);
     };
 }
 
