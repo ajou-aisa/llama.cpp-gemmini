b075fb8055f6fda4678ac6e1fcad03c98917a610
zerogod <code0.sin@gmail.com>
2025-11-06 18:32:31 +0900

    feat: set debug logs to fprint stderr

--- tools/main/main.cpp
+++ tools/main/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char ** argv) {
     }
 
     uint64_t start_cycle = read_cycles();
-    printf("start cycle : %lu \n", start_cycle);
+    fprintf(stderr, "start cycle : %lu \n", start_cycle);
     common_init();
 
     auto & sparams = params.sampling;
@@ -976,7 +976,7 @@ int main(int argc, char ** argv) {
     ggml_threadpool_free_fn(threadpool_batch);
 
     uint64_t end_cycle = read_cycles();
-    printf("end cycle : %lu \n", end_cycle);
-    printf("total elapsed cycle : %lu \n", end_cycle - start_cycle);
+    fprintf(stderr, "end cycle : %lu \n", end_cycle);
+    fprintf(stderr, "total elapsed cycle : %lu \n", end_cycle - start_cycle);
     return 0;
 }
