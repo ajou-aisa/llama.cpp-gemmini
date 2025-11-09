c7f63c27d061f1b35a9a430dd8e83e29b582f6a4
zerogod <code0.sin@gmail.com>
2025-11-09 13:29:56 +0900

    chore: make comment the files do not use anymore

--- ggml/src/ggml-gemmini/gemmini_tensor/gemmini_tensor_interface.h
+++ ggml/src/ggml-gemmini/gemmini_tensor/gemmini_tensor_interface.h
@@ -1,18 +1,18 @@
-//gemmini_tensor/gemmini_tensor_interface.h
-#pragma once
+// //gemmini_tensor/gemmini_tensor_interface.h
+// #pragma once
 
-#if USE_GEMMINI_BENCH_TENSOR
-    // Target(Bench) Tensor 사용
-    #include "bench_tensor/bench_tensor.h"
-    namespace aisa {
-        template<typename T>
-        using GemminiTensor = BenchTensor<T>;
-    }
-#else
-    // Baseline Tensor 사용
-    #include "baseline_tensor/baseline_tensor.h" 
-    namespace aisa {
-        template<typename T>
-        using GemminiTensor = BaselineTensor<T>;
-    }
-#endif
+// #if USE_GEMMINI_BENCH_TENSOR
+//     // Target(Bench) Tensor 사용
+//     #include "bench_tensor/bench_tensor.h"
+//     namespace aisa {
+//         template<typename T>
+//         using GemminiTensor = BenchTensor<T>;
+//     }
+// #else
+//     // Baseline Tensor 사용
+//     #include "baseline_tensor/baseline_tensor.h" 
+//     namespace aisa {
+//         template<typename T>
+//         using GemminiTensor = BaselineTensor<T>;
+//     }
+// #endif
