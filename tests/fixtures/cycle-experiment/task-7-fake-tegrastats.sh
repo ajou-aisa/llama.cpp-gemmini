194d130d24c7c67307295fb3d5f0ae1d0618eace
Chan <mojoid0913@naver.com>
2026-09-09 14:05:48 +0900

    chore(experiment): remove cycle matrix and frequency tooling

--- tests/fixtures/cycle-experiment/task-7-fake-tegrastats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-trap 'exit 0' INT TERM
-[[ -z ${FAKE_TEGRASTATS_PID_FILE:-} ]] || printf '%s\n' "$$" > "$FAKE_TEGRASTATS_PID_FILE"
-printf 'RAM 1/2MB CPU [ 10%%@729 , 20%%@918 ] GR3D_FREQ 0%%\n'
-while :; do
-    wait
-done
